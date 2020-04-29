3bfc7848e2550f75bf5c421cc94e90bcf93cd7b3
Jagger <huynhdat2703@gmail.com>
2020-04-29 21:13:09 +0700

    01 - Setup Express

---
+++ 01-Setup Express/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('Server is running on port:', port);
+});
+
+app.get('/', (request, response) => {
+    response.send("Hello ExpressJS");
+});
+
+app.get('/users', (request, response) => {
+    response.send("This is list user!");
+});
