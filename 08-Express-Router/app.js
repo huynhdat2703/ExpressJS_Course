4a09d69f567526c617e7366c3999a12efcf72921
Jagger <huynhdat2703@gmail.com>
2020-05-09 15:44:46 +0700

    08 - Express Router

---
+++ 08-Express-Router/app.js
@@ -0,0 +1,56 @@
+/**
+ * Config Express Server
+ */
+const express = require('express');
+const app = express();
+const port = 3000;
+
+/**
+ * Config Template Engine
+ */
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+/**
+ * Import Module Body-parser
+ */
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Import User Route
+ */
+const userRoute = require('./routes/user.route');
+app.use('/user', userRoute);
+/**
+ * Start Server
+ */
+app.listen(port, () => {
+    console.log("Server is running on port", port);
+});
+
+/**
+ * Index Page
+ */
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "08-Express-Router"
+    });
+});
+
+/**
+ * Login Page
+ */
+app.post('/login', (req, res) => {
+    var loginUser = req.body;
+    if(loginUser.username === "admin" && loginUser.password === "123") {
+        res.redirect('/user');
+    }
+    else {
+        res.render('index', {
+            title: "08-Express-Router",
+            errorMessage: "Username or Password is wrong!"
+        });
+    }
+});
